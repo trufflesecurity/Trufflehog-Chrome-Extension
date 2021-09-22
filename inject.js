29d1ca40317347eeac205e1d89f8105255bf9c92
Massimo Prencipe <massimo.prencipe@solita.fi>
2021-09-22 21:21:17 +0300

    Format files with VSCode JS/CSS/HTML formatter

--- inject.js
+++ inject.js
@@ -1,36 +1,31 @@
 // this is the code which will be injected into a given page...
 
-(function(){
+(function () {
 
     let re = new RegExp();
     let match = "";
     let page = document.documentElement.innerHTML;
-    chrome.runtime.sendMessage({"pageBody": page, "origin":window.origin, "parentUrl": window.location.href, "parentOrigin": window.origin});
+    chrome.runtime.sendMessage({ "pageBody": page, "origin": window.origin, "parentUrl": window.location.href, "parentOrigin": window.origin });
 
-    //chrome.browserAction.setBadgeText({text: 'ON'});
-
-
-
-
-    setTimeout(function(){
-         for (scriptIndex in document.scripts) {
-            if (document.scripts[scriptIndex].src){
+    setTimeout(function () {
+        for (scriptIndex in document.scripts) {
+            if (document.scripts[scriptIndex].src) {
                 let scriptSRC = document.scripts[scriptIndex].src;
-                if (scriptSRC.startsWith("//")){
+                if (scriptSRC.startsWith("//")) {
                     scriptSRC = location.protocol + scriptSRC
                 }
-                chrome.runtime.sendMessage({"scriptUrl": scriptSRC, "parentUrl": window.location.href, "parentOrigin": window.origin});
+                chrome.runtime.sendMessage({ "scriptUrl": scriptSRC, "parentUrl": window.location.href, "parentOrigin": window.origin });
             }
 
         }
-    },2000)
+    }, 2000)
     let origin = window.location.origin;
     let originalPath = window.location.pathname;
     let newPath = originalPath.substr(0, originalPath.lastIndexOf("/"));
     let newHref = origin + newPath;
     let envUrl = newHref + "/.env";
-    chrome.runtime.sendMessage({"envFile": envUrl,"parentUrl": window.location.href, "parentOrigin": window.origin});
+    chrome.runtime.sendMessage({ "envFile": envUrl, "parentUrl": window.location.href, "parentOrigin": window.origin });
     let gitUrl = newHref + "/.git/config";
-    chrome.runtime.sendMessage({"gitDir": gitUrl, "parentUrl": window.location.href, "parentOrigin": window.origin});
+    chrome.runtime.sendMessage({ "gitDir": gitUrl, "parentUrl": window.location.href, "parentOrigin": window.origin });
 })()
 
